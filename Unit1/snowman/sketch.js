98b8b3135da378b97696948d6186c14211787ffd
jastohr <69992045+jastohr@users.noreply.github.com>
2021-01-19 10:08:45 -0600

    snowman

---
+++ Unit1/snowman/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+background('white');
+ellipse(width/2, height/2, 50, 50);
+ellipse(width/2, height/2, 100, 100);
+ellipse(width/2, height/2 + 75, 200, 200);
+}
